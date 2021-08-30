b596e17ca430e27c8b6bf58565529ddd9a98b374
Marcelo Agoston <marceloagoston91@gmail.com>
2021-08-29 23:10:23 -0300

    Agregar Contador de usuarios registrados en admin

--- src/Apps/pages/views.py
+++ src/Apps/pages/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import TemplateView
 
+from Apps.usuarios.models import Usuario
+
 class HomePageView(TemplateView):
     template_name = 'home.html'
 
 class AdminHomePageView(TemplateView):
     template_name = 'admin/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['usuariosRegistrados'] = Usuario.objects.all().count()
+
+        return context
