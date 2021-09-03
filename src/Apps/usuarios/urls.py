a2e4c894d229bd2dd29ae172ad22f78b79466295
Marcelo Agoston <marceloagoston91@gmail.com>
2021-09-03 10:15:04 -0300

    Agregar pantalla Lista de Usuarios

--- src/Apps/usuarios/urls.py
+++ src/Apps/usuarios/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('registro/', views.RegistroView.as_view(), name='registro'),
+    path('lista-usuarios/', views.UsuarioListView.as_view(), name='lista_usuarios'),
+
 ]
--- src/Apps/usuarios/views.py
+++ src/Apps/usuarios/views.py
@@ -1,8 +1,19 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView 
+from django.views.generic.edit import UpdateView, DeleteView 
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+
+from .models import Usuario
 from .forms import UsuarioCreationForm
+
 
 class RegistroView(CreateView):
     form_class = UsuarioCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/registro.html'
+
+class UsuarioListView(LoginRequiredMixin, ListView):
+    model = Usuario
+    template_name = 'admin/usuarios/listausuarios.html'
+    context_object_name = 'listaUsuarios'
+    login_url = 'login'
